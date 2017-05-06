4ced5ff6d5870583385bcb9fd7e41d88519ceda2
Babatunde Adeyemi <barbietunnie@gmail.com>
2017-05-06 20:21:33 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, now!');
+});
+
+app.listen(3000, () => {
+    console.log('Sample app listening on port 3000!');
+});
